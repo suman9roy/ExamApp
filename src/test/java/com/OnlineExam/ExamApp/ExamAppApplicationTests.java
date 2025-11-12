edcd0383f89b950c4250557748a0cbc76d7945a2
suman9roy <suman99999roy@gmail.com>
2025-11-13 00:43:54 +0530

    initial Commit

---
+++ src/test/java/com/OnlineExam/ExamApp/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineExam.ExamApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
